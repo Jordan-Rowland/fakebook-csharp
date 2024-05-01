85ebeda17aab9a0f3fec70e20f5409a6a297745a
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2024-04-30 20:07:39 -0700

    project scaffold

--- Program.cs
+++ Program.cs
@@ -1,24 +1,92 @@
+using Asp.Versioning;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc(
+        "v1",
+        new OpenApiInfo { Title = "Fakebook", Version = "v1.0" });
+    options.SwaggerDoc(
+        "v2",
+        new OpenApiInfo { Title = "Fakebook", Version = "v2.0" });
+});
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(cfg =>
+    {
+        cfg.WithOrigins(builder.Configuration["AllowedOrigins"]!);
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+    options.AddPolicy(name: "AnyOrigin", cfg =>
+    {
+        cfg.AllowAnyOrigin();
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+    options.AddPolicy(name: "AnyOrigin_GetOnly", cfg =>
+    {
+        cfg.AllowAnyOrigin();
+        cfg.AllowAnyHeader();
+        cfg.WithMethods("GET");
+    });
+});
+
+builder.Services.AddApiVersioning(options => {
+    options.ApiVersionReader = new UrlSegmentApiVersionReader();
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+})
+.AddMvc()
+.AddApiExplorer(options => {
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Configuration.GetValue<bool>("UseSwagger"))
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint(
+            $"/swagger/v1/swagger.json",
+            $"Fakebook v1");
+        options.SwaggerEndpoint(
+            $"/swagger/v2/swagger.json",
+            $"Fakebook v2");
+    });
 }
 
-app.UseHttpsRedirection();
+if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
+    app.UseDeveloperExceptionPage();
+else
+    app.UseExceptionHandler("/error");
 
+app.UseHttpsRedirection();
+app.UseCors();
 app.UseAuthorization();
+
+app.MapGet("/error",
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    [EnableCors("AnyOrigin")]
+    [ResponseCache(NoStore = true)] () =>
+        Results.Problem());
+
+app.MapGet("/error/test",
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    [EnableCors("AnyOrigin_GetOnly")]
+    [ResponseCache(NoStore = true)] () =>
+        { throw new Exception("test"); });
 
 app.MapControllers();
 
